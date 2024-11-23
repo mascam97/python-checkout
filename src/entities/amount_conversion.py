af932b778eec52d52b02f0a8ea4fc81b0e8a4e04
Iván Andrés López G. <andresgom11012@gmail.com>
2024-11-23 16:39:11 -0500

    test: adds  amount_conversion test case

--- src/entities/amount_conversion.py
+++ src/entities/amount_conversion.py
@@ -5,15 +5,14 @@ from entities.amount_base import AmountBase
 
 class AmountConversion(BaseModel):
     fromAmount: Optional[AmountBase] = Field(
-        default=None, alias="from", description="Base amount to convert from"
+        default=None, description="Base amount to convert from"
     )
     toAmount: Optional[AmountBase] = Field(
-        default=None, alias="to", description="Base amount to convert to"
+        default=None, description="Base amount to convert to"
     )
     factor: float = Field(default=1.0, description="Conversion factor")
-    model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True)
 
-    def set_amount_base(self, base: dict) -> None:
+    def set_amount_base(self, base: dict[AmountBase]) -> None:
         """
         Quickly set all values to the same base.
         """
@@ -27,7 +26,7 @@ class AmountConversion(BaseModel):
         Convert the AmountConversion object to a dictionary.
         """
         return {
-            "from": self.fromAmount.to_dict() if self.fromAmount else None,
-            "to": self.toAmount.to_dict() if self.toAmount else None,
+            "fromAmount": self.fromAmount.to_dict() if self.fromAmount else None,
+            "toAmount": self.toAmount.to_dict() if self.toAmount else None,
             "factor": self.factor,
         }
