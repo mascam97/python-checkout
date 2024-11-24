401a3bdf2a5736e219e1308120a00de5edc1fefd
ivan andres <andresgom11012@gmail.com>
2024-11-23 21:53:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup, find_packages
-import os
-
-long_description = ""
-readme_path = os.path.join(os.path.dirname(__file__), "README.md")
-
-if os.path.exists(readme_path):
-    with open(readme_path, "r", encoding="utf-8") as fh:
-        long_description = fh.read()
         
 setup(
     name="p2p_checkout",
@@ -39,7 +31,7 @@ setup(
         ],
     },
     description="Python library for PlaceToPay integration.",
-    long_description=long_description,
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Iván Andrés López Gómez",
     author_email="ialopez11012@gmail.com",
