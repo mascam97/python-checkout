cef666f47491550811aba0e7e6cba2561a6a5a20
ivan andres <andresgom11012@gmail.com>
2024-12-08 18:21:49 -0500

    some refactoring

---
+++ checkout/__init__.py
@@ -0,0 +1,9 @@
+from checkout.checkout import Checkout
+from checkout.messages.requests.redirect import RedirectRequest
+from checkout.messages.requests.collect import CollectRequest
+
+__all__=[
+    "Checkout"
+    "RedirectRequest",
+    "CollectRequest"
+]
