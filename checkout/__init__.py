d2fee83add59812408c99a59167ed7b9489546d7
ivan andres <andresgom11012@gmail.com>
2024-12-08 22:39:30 -0500

    Update __init__.py

--- checkout/__init__.py
+++ checkout/__init__.py
@@ -2,8 +2,4 @@ from checkout.checkout import Checkout
 from checkout.messages.requests.redirect import RedirectRequest
 from checkout.messages.requests.collect import CollectRequest
 
-__all__=[
-    "Checkout"
-    "RedirectRequest",
-    "CollectRequest"
-]
+__all__ = ["Checkout", "RedirectRequest", "CollectRequest"]
